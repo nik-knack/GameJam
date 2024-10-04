30ac2601bfa171613b32125d2d524a972bf7cbfa
NFX-Prime <fn2926@rit.edu>
2024-10-04 18:09:19 -0400

    Tamagotchi class created

---
+++ Tama_Caretaker/Tamagotchi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tama_Caretaker
+{
+    internal class Tamagotchi
+    {
+        private const int SLEEPDECREMENT = 7;
+        private const int HUNGERDECREMENT = 5;
+
+        public int sleepBarFrame;
+        public int hungerBarFrame;
+
+
+        public Tamagotchi()
+        {
+            sleepBarFrame = 7;
+            hungerBarFrame = 7;
+        }
+    }
+}
