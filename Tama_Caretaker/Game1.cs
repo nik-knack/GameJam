c75082be2a5ff40524a8728f0b49df0b19baa367
Dania Macias <dlm4695@rit.edu>
2024-10-04 19:23:06 -0400

    viewport size change

--- Tama_Caretaker/Game1.cs
+++ Tama_Caretaker/Game1.cs
@@ -22,6 +22,10 @@ namespace Tama_Caretaker
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _graphics.IsFullScreen = false;
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
